fe346a2e04d5849caea73cc06c158bc78d45f6a2
Rinku Sharma <rsharma3@xavient.com>
2016-06-03 18:14:55 +0530

    Converting image content into String

---
+++ src/main/java/com/imagecontent/reader/ImageContentReader.java
@@ -0,0 +1,33 @@
+package com.imagecontent.reader;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
+
+public class ImageContentReader {
+
+	public static void main(String[] args) {
+		File imageFile = new File("d://love.jpg");
+		String imageContent = imageToStringConvertor(imageFile);
+		System.out.println(imageContent);
+	}
+
+	public static String imageToStringConvertor(File imageFile) {
+		String result = null;
+		ITesseract instance = new Tesseract();
+		instance.setDatapath(LoadLibs.extractTessResources("tessdata")
+				.getAbsolutePath());
+
+		try {
+			result = instance.doOCR(imageFile);
+		} catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return result;
+	}
+
+}
---
+++ src/main/java/com/imagecontent/reader/ImageContentReaderApplication.java
@@ -0,0 +1,12 @@
+package com.imagecontent.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageContentReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageContentReaderApplication.class, args);
+	}
+}
